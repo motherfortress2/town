93fc0f6284f63cebd0efb61a4dc9f7533f64e073
SpaceWD <121475790+motherfortress2@users.noreply.github.com>
2024-10-06 22:06:17 -0400

    Update views.js

--- views.js
+++ views.js
@@ -1,3 +1,11 @@
 fetch('./town.json')
     .then((response) => response.json())
     .then((json) => console.log(json));
+
+fetch('./town-pack.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+fetch('./town-physics.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
